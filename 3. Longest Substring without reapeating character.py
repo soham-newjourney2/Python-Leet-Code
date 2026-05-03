b5dd14f3ddb6e45d88d0290edbbc397b7097fc49
Soham Chowdhury <soham.chowdhury.2026@gmail.com>
2026-05-03 17:41:00 +0530

    Add files via upload

---
+++ 3. Longest Substring without reapeating character.py
@@ -0,0 +1,16 @@
+# Given a string s, find the length of the longest substring without duplicate characters.
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_set = set()
+        left = 0
+        max_len = 0
+        
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+            char_set.add(s[right])
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
